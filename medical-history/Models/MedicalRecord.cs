e6ce23d9e51c6a53462d3387a3f4acfbcc51c49e
Johana Elizalde <134255446+jelizalde04@users.noreply.github.com>
2025-06-22 21:42:18 -0500

    feat (models): ✨ update Pet and MedicalRecord models with new properties

---
+++ medical-history/Models/MedicalRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace medical_history.Models
+{
+    public class MedicalRecord
+    {
+        public Guid Id { get; set; }
+        public Guid PetId { get; set; }
+        public DateTime LastVisitDate { get; set; }
+        public float Weight { get; set; }
+        public string HealthStatus { get; set; }
+        public string Diseases { get; set; }
+    }
+}
+
---
+++ medical-history/Models/Pet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace medical_history.Models
+{
+    public class Pet
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public string Breed { get; set; }
+        public string Image { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Residence { get; set; }
+        public string Gender { get; set; }
+        public string Color { get; set; }
+        public Guid ResponsibleId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
