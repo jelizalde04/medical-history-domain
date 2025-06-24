fdbbbc517c2c94e8a46475adfa9f57f6c4cfef8c
Johana Elizalde <134255446+jelizalde04@users.noreply.github.com>
2025-06-24 03:21:49 -0500

    feat(database): 🗄️ add PetMedicalContext for medical records

---
+++ medical-record/medical-record/Data/PetMedicalContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetMedicalHistoryAPI.Models;
+
+namespace PetMedicalHistoryAPI.Data
+{
+    public class PetMedicalContext : DbContext
+    {
+        public PetMedicalContext(DbContextOptions<PetMedicalContext> options) : base(options) { }
+
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<MedicalRecord> MedicalRecords { get; set; }
+    }
+}
